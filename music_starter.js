f4bc10babe8db8594f50c0c3dccd6c937786415b
Cameron Wilkinson <22cameron.w@gmail.com>
2025-08-12 15:50:31 +1200

    Update on my work again
    I have no added more legs and 2 new drums that sit in the middle, they both move acording to the drum width is looking good, I just have to make sure the sizes are correct all thoughout the song so it doesnt look weird at any point. My next step will be adding the next set of drums and matching them to the drum sounds as well.

--- music_starter.js
+++ music_starter.js
@@ -14,7 +14,8 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
 
   
 
-  let sizeMap = map(bass, 0, 100, 200, 300);
+  let goldPlateSize = map(bass, 0, 100, 220, 250);
+  let redDrum = map(drum, 0, 100, 120, 150);
 
   //Top left gold drum
   drumLegs(400, 120); //top left drum leg
@@ -29,26 +30,27 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
   drumLegs(830, 415); //top left drum leg
   
 
-
   
+  creamyDrums(midX - 20, midY, redDrum);
+  creamyDrums(midX + 120, midY, redDrum);
 
 
   //base plates
   noStroke();
+  fill(231, 175, 64, 255);
+  ellipse(midX - 150, midY - 100, goldPlateSize); // top left
+  ellipse(midX + 250, midY - 100, goldPlateSize); // top right
   fill(231, 175, 64);
-  ellipse(midX - 150, midY - 100, sizeMap); // top left
-  ellipse(midX + 250, midY - 100, sizeMap); // top right
-  fill(231, 175, 64);
-  ellipse(midX - 200, midY + 150, (sizeMap) * 0.9); // bottom left 
-  ellipse(midX + 300, midY + 150, (sizeMap) * 0.9); // bottom right 
+  ellipse(midX - 200, midY + 150, (goldPlateSize) * 0.9); // bottom left 
+  ellipse(midX + 300, midY + 150, (goldPlateSize) * 0.9); // bottom right 
   
 
-  orangePlateTopLeft(midX, midY, sizeMap);
-  orangePlateBottomLeft(midX, midY, sizeMap);
+  orangePlateTopLeft(midX, midY, goldPlateSize);
+  orangePlateBottomLeft(midX, midY, goldPlateSize);
 
-  orangePlateTopLeft(midX + 400, midY, sizeMap);
-  orangePlateBottomLeft(midX + 500, midY, sizeMap);
-   
+  orangePlateTopLeft(midX + 400, midY, goldPlateSize);
+  orangePlateBottomLeft(midX + 500, midY, goldPlateSize);
+  
 
   
 
@@ -62,19 +64,20 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
 
 function drumLegs(x, y) {
 
-  fill("black")
+  fill("black");
+  stroke("grey");
   rect(x - 25 , y - 35, 30, 30);
   noStroke();
 
-  fill("grey")
+  fill("grey");
   rect(x - 20 , y - 30, 20, 20);
   noStroke();
   
   stroke("black");
-  noFill()
+  noFill();
   strokeWeight(15);
+  line(x + 30, y + 35, x - 10, y - 20);
   
-  line(x + 30, y + 60, x - 10, y - 20);
   strokeWeight(2);
   
   
@@ -83,13 +86,13 @@ function drumLegs(x, y) {
 }
 
 
-function orangePlateTopLeft(midX, midY, sizeMap) {
+function orangePlateTopLeft(midX, midY, goldPlateSize) {
 
   let spacing = 30;
   for (let i = 1; i <= 5; i++) {
-    stroke(209, 148, 33);
+    stroke(205, 152, 50, 255);
     noFill();
-    let diameter = sizeMap - i * spacing;
+    let diameter = goldPlateSize - i * spacing;
     ellipse(midX - 150, midY - 100, diameter);
 
   }
@@ -115,13 +118,13 @@ function orangePlateTopLeft(midX, midY, sizeMap) {
 
 }
 
-function orangePlateBottomLeft(midX, midY, sizeMap) {
+function orangePlateBottomLeft(midX, midY, goldPlateSize) {
   let spacing = 30;
 
   for (let i = 1; i <= 5; i++) {
     stroke(209, 148, 33);
     noFill();
-    let diameter = sizeMap * 0.9 - i * spacing;
+    let diameter = goldPlateSize * 0.9 - i * spacing;
     ellipse(midX - 200, midY + 150, diameter);
   }
   
@@ -141,6 +144,27 @@ function orangePlateBottomLeft(midX, midY, sizeMap) {
   fill("white");
   noStroke();
   ellipse(midX - 200, midY + 150, 5);
+
+
+
+}
+
+function creamyDrums(x, y, redDrum) {
+  strokeWeight(5);
+  stroke(200, 203, 218, 255); // grey drum outline
+  fill(228, 31, 38, 255); //red
+  ellipse(x, y - 40, redDrum - 20);
+
+  strokeWeight(8);
+  stroke(200, 203, 218, 255); // grey drum outline
+  fill(249, 249, 237, 255); // lighter grey, bigger circle
+  ellipse(x, y - 10, redDrum - 20);
+
+  strokeWeight(2);
+  noStroke();
+  fill(216,216,208,255); // darkest grey small circle
+  ellipse(x, y - 10, redDrum - 80); 
+
 
 }
 
